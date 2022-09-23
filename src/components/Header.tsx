f98b0bc48cbddc5d45699ec863218affca4b0142
Manan Tank <manantankm@gmail.com>
2022-09-23 19:25:08 +0530

    add dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,29 @@
-import { InfoIcon, githubIcon } from '../icons';
+import { useState } from 'react';
+import { InfoIcon, githubIcon, sun, moon } from '../icons';
 import styles from '../styles/Header.module.css';
 
 export function Header() {
+	const [isLightTheme, setIsLightTheme] = useState(true);
 	return (
 		<header className={styles.header}>
 			<div className={styles.logoContainer}>
 				<h1 className={styles.crux}> CrUX </h1>
 			</div>
 			<div className={styles.links}>
+				<button
+					aria-label='Switch Theme'
+					className={styles.themeSwitcher}
+					onClick={() => {
+						if (isLightTheme) {
+							document.body.classList.add('dark-mode');
+						} else {
+							document.body.classList.remove('dark-mode');
+						}
+						setIsLightTheme(!isLightTheme);
+					}}>
+					{isLightTheme ? sun : moon}
+				</button>
+
 				<a
 					href='https://developer.chrome.com/docs/crux/about/'
 					target='_blank'
